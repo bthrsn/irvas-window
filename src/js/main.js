ab9469549ae4e870a81d0105649003ff5d4be60d
Mikhail <mikhail.lhts@gmail.com>
2020-10-03 20:55:03 +0300

    add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 import './slider';
 import {modal, openModal, closeModal} from './modules/modal';
+import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
   // значение переменной, через сколько времени на сайте откроется модальное окно
-
   const modalTimerId = setTimeout(() => openModal('.popup', modalTimerId), 60000);
 
   modal('.popup_engineer', '.popup_engineer_btn', '.popup_engineer_close');
   modal('.popup', '.phone_link', '.popup_close');
+
+  tabs('.glazing_block', '.glazing_content', '.glazing_slider', 'active');
+  tabs('.no_click', '.decoration_content > div > div', '.decoration_slider', 'after_click');
+
 })
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,41 @@
+const tabs = (tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) => {
+
+  const tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(tabsContentSelector),
+        tabsParent = document.querySelector(tabsParentSelector);
+
+  const hideTabContent = () => {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+
+    tabs.forEach(tab => tab.classList.remove(activeClass));
+  }
+
+  const showTabContent = (i = 0) => {
+    tabsContent[i].style.display = 'block';
+
+    tabs[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && 
+      (target.classList.contains(tabsSelector.replace(/\./, '')) ||
+       (target.parentNode.classList.contains(tabsSelector.replace(/\./, ''))))) {
+      tabs.forEach((tab, i) => {
+        if (target === tab || target.parentNode === tab) {
+          hideTabContent();
+          showTabContent(i);
+        };
+      });
+    }
+  });
+
+};
+
+export default tabs;
